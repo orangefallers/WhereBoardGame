b4f1db662af3c9cda121be8526dd173f2188e201
orangefaller <orangefallers>
2017-02-05 11:20:41 +0800

    1.add report activity and layout 2.modify welcome button style

---
+++ app/src/main/java/com/ofcat/whereboardgame/report/ReportActivity.java
@@ -0,0 +1,20 @@
+package com.ofcat.whereboardgame.report;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ofcat.whereboardgame.R;
+
+/**
+ * Created by orangefaller on 2017/2/5.
+ */
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
