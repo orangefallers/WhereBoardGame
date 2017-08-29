8e060591ced35695fa2c78f8f53902b938c69914
Kevin_Hsieh <kevin_hsieh@Kevin-Hsiehde-MacBook-Pro.local>
2017-08-29 17:24:03 +0800

    add new task for get latlng

---
+++ app/src/main/java/com/ofcat/whereboardgame/task/GetAddressToLatLngTask.java
@@ -0,0 +1,14 @@
+package com.ofcat.whereboardgame.task;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by kevin_hsieh on 2017/8/29.
+ */
+
+public class GetAddressToLatLngTask extends AsyncTask<Object,Object,String> {
+    @Override
+    protected String doInBackground(Object... params) {
+        return null;
+    }
+}
